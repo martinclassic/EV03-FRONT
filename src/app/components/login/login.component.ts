54a8c74ed19076ce071dab1ea887d2d4fcc37fbf
martinclassic <martin.alonsopt@gmail.com>
2026-07-12 18:07:17 -0400

    proyecto angular

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  usuario: string = '';
+  password: string = '';
+  mensaje: string = '';
+
+  constructor(private router: Router){}
+
+  ingresar(){
+
+    if(this.usuario === 'admin' && this.password === '1234'){
+
+      this.router.navigate(['/formulario']);
+
+    }else{
+
+      this.mensaje = 'Usuario o contraseña incorrectos';
+
+    }
+
+  }
+
+}
